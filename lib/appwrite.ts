24a24c4605947825ef6a4ab340ca69f0adddade6
Lokesh_Saravanan <Lokesh.Saravanan@analyticbrains.com>
2025-02-26 15:20:53 +0530

    Implement login and logout functionality with Appwrite; add user retrieval method

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,5 +1,6 @@
 import { Client, Avatars, Account, OAuthProvider } from "react-native-appwrite";
 import * as Linking from "expo-linking";
+import { openAuthSessionAsync } from "expo-web-browser";
 export const config = {
   Platform: "com.jsm.restate",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
@@ -26,8 +27,55 @@ export async function login() {
     );
 
     if (!response) throw new Error("Failed to login");
+
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+
+    if (browserResult.type !== "success") throw new Error("Failed to login");
+
+    const url = new URL(browserResult.url);
+
+    const secret = url.searchParams.get("secret")?.toString();
+    const userId = url.searchParams.get("userId")?.toString();
+
+    if (!secret || !userId) throw new Error("Failed to login");
+
+    const session = await account.createSession(userId, secret);
+
+    if (!session) throw new Error("Failed to create session");
+
+    return true;
   } catch (ex) {
     console.error(ex);
     return false;
+  }
+}
+
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function getUser() {
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
+    }
+    return response;
+  } catch (ex) {
+    console.error(ex);
+    return null;
   }
 }
