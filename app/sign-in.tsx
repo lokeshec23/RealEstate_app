9625d486b97e450f135b65860c3dc83964e5be8e
Lokesh_Saravanan <Lokesh.Saravanan@analyticbrains.com>
2025-02-26 15:54:57 +0530

    Add global context provider and layout handling for authentication state

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,36 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
+import { login } from "@/lib/appwrite";
+import { useGlobalContext } from "@/lib/global-provider";
+import { Redirect } from "expo-router";
 
 const signIn = () => {
-  const handleLogin = () => {};
+  const { refetch, loading, isLoggedIn } = useGlobalContext();
+
+  const handleLogin = async () => {
+    try {
+      const result = await login();
+
+      if (result) {
+        refetch();
+        console.log("Login Success");
+      } else {
+        Alert.alert("Error", "Failed to login");
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
   return (
     <SafeAreaView className="bg-white h-full">
       <ScrollView contentContainerClassName="h-full">
