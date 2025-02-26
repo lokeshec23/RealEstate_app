7e3a5643e0428f4f1f0112fe09c2f8e2b3439743
Lokesh_Saravanan <Lokesh.Saravanan@analyticbrains.com>
2025-02-26 13:49:37 +0530

    Add Appwrite integration and update dependencies

---
+++ lib/appwrite.ts
@@ -0,0 +1,33 @@
+import { Client, Avatars, Account, OAuthProvider } from "react-native-appwrite";
+import * as Linking from "expo-linking";
+export const config = {
+  Platform: "com.jsm.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+};
+
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.Platform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+
+    if (!response) throw new Error("Failed to login");
+  } catch (ex) {
+    console.error(ex);
+    return false;
+  }
+}
